e30b439788c4862f1218f63d19092cf391b841e8
RTG <igadget28@gmail.com>
2017-10-29 16:19:29 +0800

    Added roll the dice

--- RollTheDice/roll.py
+++ RollTheDice/roll.py
@@ -10,45 +10,16 @@ def end():
 
 
 def main():
-    Tries = 0
-    sides = 6
     rolling = True
+    sides = 6
     while rolling:
-        roll_again = input("Ready to roll again? ENTER = Roll Q = QUIT ")
+        roll_again = input("Ready to roll? ENTER = Roll Q = QUIT ")
         if roll_again.lower() != "q":
-            num_rolled = int(input("Guess a number: "))
-            if num_rolled > roll(6):
-                print("You rolled higher, please try again!")
-                Tries += 1
-                ask = input("You wanna try again? ENTER = Again Q = QUIT ")
-                if ask.lower() == "q":
-                    end()
-                    print("You took " + str(Tries) + " tries!")
-                else:
-                    main()
-            else:
-                if num_rolled < roll(6):
-                    print("You rolled lower, please try again!")
-                    Tries += 1
-                    ask = input("You wanna try again? ENTER = Again Q = QUIT ")
-                    if ask.lower() == "q":
-                        end()
-                        print("You took " + str(Tries) + " tries!")
-                    else:
-                        main()
-            if num_rolled == roll(6):
-                print("WOAH, you got the right roll! Nice try!")
-                Tries += 1
-                ask = input("You wanna try again? ENTER = Again Q = QUIT ")
-                if ask.lower() == "q":
-                    end()
-                    print("You took " + str(Tries) + " tries!")
-                else:
-                    main()
+            num_rolled = roll(sides)
+            print("You rolled a", num_rolled)
+            end()
         else:
             rolling = False
-
-    print("Thanks for playing!")
 
 
 main()
